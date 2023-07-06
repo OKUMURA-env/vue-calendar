7ebc05211356f9fda82f724941fd7016f27f9449
OKUMURA-env <n.okumura@env-value.co.jp>
2023-07-06 19:10:25 +0900

    不要な記述を削除

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,7 @@
 import './bootstrap';
 import router from './routes';
 import { createApp } from 'vue/dist/vue.esm-bundler.js';
-import CalendarComponent from './components/Calendar.vue';
 
 const app = createApp({})
 app.use(router)
-app.component('calendar', CalendarComponent);
 app.mount('#app');
